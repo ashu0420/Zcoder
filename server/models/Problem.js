1295e86ab27474837de02c093a1f5c6c3e881459
ashu <asheesh.iitg@gmail.com>
2026-06-17 08:55:20 +0530

    modified:   server/models/Problem.js     improving schema of problem

--- server/models/Problem.js
+++ server/models/Problem.js
@@ -3,16 +3,30 @@ const mongoose = require("mongoose");
 const problemSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: true,
+        required: true
     },
-    difficulty: {
+    slug: {
         type: String,
-        enum: ["Easy", "Medium", "Hard"],
         required: true,
+        unique: true
     },
-    testCases: {
-        type: Array,
-        required:true,
-    }
+    difficulty: String,
+    description: String,
+    examples: Array,
+    constraints: [String],
+    tags: [String],
+    starterCode: String,
+    testCases: Array
 });
+
+problemSchema.pre("save", function (next) {
+    if (!this.slug) {
+        this.slug = this.title
+            .toLowerCase()
+            .replace(/[^a-z0-9]+/g, "-")
+            .replace(/^-|-$/g, "");
+    }
+    next();
+});
+
 module.exports = mongoose.model("Problem", problemSchema);
